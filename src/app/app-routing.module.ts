2c55406981c9b9b6a19d673ad4ef49a5bb33f4a7
Sanket Chaudhary <sanketgodara140197@gmail.com>
2018-07-19 18:24:45 +0530

    almost completed. middle pages pending.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { UpdateComponent } from './update/update.component';
+import { FaqComponent } from './faq/faq.component';
+import { UpdatestatusComponent } from './updatestatus/updatestatus.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: HomepageComponent},
+  { path:'updateportal', component: UpdateComponent},
+  { path:'faq', component: FaqComponent},
+  { path:'updatestatus', component: UpdatestatusComponent},
+  { path:'instructions', component: InstructionsComponent},
+  { path:'contactus', component: ContactusComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-import { MatStepperModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatOptionModule, MatSelectModule, MatInputModule, MatSidenavModule, MatMenuModule, MatCheckboxModule } from '@angular/material';
+import { MatStepperModule, MatIconModule, MatButtonModule, MatGridListModule, MatFormFieldModule, MatOptionModule, MatSelectModule,
+   MatInputModule, MatSidenavModule, MatMenuModule, MatCheckboxModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,6 +18,11 @@ import { StepTwoComponent } from './step-two/step-two.component';
 import { StepThreeComponent } from './step-three/step-three.component';
 import { StepFourComponent } from './step-four/step-four.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomepageComponent } from './homepage/homepage.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { UpdatestatusComponent } from './updatestatus/updatestatus.component';
 
 @NgModule({
   declarations: [
@@ -26,11 +32,18 @@ import { AppRoutingModule } from './/app-routing.module';
     StepOneComponent,
     StepTwoComponent,
     StepThreeComponent,
-    StepFourComponent
+    StepFourComponent,
+    HomepageComponent,
+    InstructionsComponent,
+    FaqComponent,
+    ContactusComponent,
+    UpdatestatusComponent
   ],
   imports: [
-    BrowserModule, MatSelectModule, BrowserAnimationsModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatToolbarModule, MatListModule, MatCardModule,
-    MatStepperModule, MatMenuModule, MatCheckboxModule, FormsModule, ReactiveFormsModule, MatOptionModule, MatSelectModule, MatInputModule, MatSidenavModule, AppRoutingModule  ],
+    BrowserModule, MatSelectModule, MatGridListModule, BrowserAnimationsModule,
+     MatIconModule, MatButtonModule, MatFormFieldModule, MatToolbarModule, MatListModule, MatCardModule,
+    MatStepperModule, MatMenuModule, MatCheckboxModule, FormsModule, ReactiveFormsModule, MatOptionModule,
+    MatSelectModule, MatInputModule, MatSidenavModule, AppRoutingModule  ],
   providers: [],
   bootstrap: [AppComponent]
 })
